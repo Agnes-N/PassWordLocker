160f4f4e0fa06c33dd0bd1594617108dd8f7cb5c
GraceUmutesi <tesigrace98@gmail.com>
2019-09-05 16:11:49 +0200

    creates codes to test if we can display credential

--- CredentialData.py
+++ CredentialData.py
@@ -11,3 +11,16 @@ class CredentialData:
         self.platform = platform
         self.username = username
         self.password = password
+
+    def save_credential(self):
+        """
+        save credential objects to the credential list
+        """
+        CredentialData.credentials.append(self)
+
+    @classmethod
+    def display_credentials(cls):
+        """
+        displays the credentials 
+        """
+        return cls.credentials
--- test_CredentialData.py
+++ test_CredentialData.py
@@ -3,24 +3,39 @@ import unittest
 from CredentialData import CredentialData
 
 
-class TestCredentials(unittest.TestCase):
+class TestCredential(unittest.TestCase):
     def setUp(self):
         """
         setUp method
         """
-        self.new_credential = CredentialData("instagram", "reina", "12345") 
+        self.new_credential = CredentialData("twitter", "aggy", "54321")
 
     def test_init(self):
         """
         testing initialization
         """
-        self.assertEqual(self.new_credential.platform, "instagram")
-        self.assertEqual(self.new_credential.username, "reina")
-        self.assertEqual(self.new_credential.password, "12345")
-    
-    def save_credential(self):
+        self.assertEqual(self.new_credential.platform, "twitter")
+        self.assertEqual(self.new_credential.username, "aggy")
+        self.assertEqual(self.new_credential.password, "54321")
 
+    def tearDown(self):
+
+        CredentialData.credentials = []
+
+    def test_save_credential(self):
         """
-        save credential objects to the credential list
+        test if credential is saved in the credentials list
         """
-        CredentialData.credentials.append(self)
+        self.new_credential.save_credential()
+        self.assertEqual(len(CredentialData.credentials), 1)
+
+    def test_display_credentials(self):
+        """
+        test display credentials method
+        """
+        self.assertEqual(CredentialData.display_credentials(),
+                        CredentialData.credentials)
+
+
+if __name__ == '__main__':
+    unittest.main()
