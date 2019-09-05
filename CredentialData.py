f900454ba5918161d319d1835dd636423cc60414
GraceUmutesi <tesigrace98@gmail.com>
2019-09-05 16:00:41 +0200

    creates codes to initialize credential instance

---
+++ CredentialData.py
@@ -0,0 +1,13 @@
+class CredentialData:
+
+    """
+    create new instances
+    """
+
+    credentials = []
+
+    def __init__(self, platform, username, password):
+
+        self.platform = platform
+        self.username = username
+        self.password = password
