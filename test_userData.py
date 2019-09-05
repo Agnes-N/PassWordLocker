fa7aa4108f8682eac0404d010ed2be8bb9c936da
GraceUmutesi <tesigrace98@gmail.com>
2019-09-05 15:44:01 +0200

    codes to test if we can create multiple username and password

--- test_userData.py
+++ test_userData.py
@@ -1,7 +1,14 @@
 import unittest
 from userData import UserData
+import pyperclip
 
 class TestuserData(unittest.TestCase):
+    '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
     def setUp(self):
         '''
         set up method
@@ -18,18 +25,29 @@ class TestuserData(unittest.TestCase):
         self.assertEqual(self.newUser.userName, "aggy")
         self.assertEqual(self.newUser.password, "12345")
 
-    def tearDown(self):
-        '''
-        Function to clean up the existing inputs
-        '''
-        UserData.createAccount = []
-
     def test_save(self):
         '''
-        A function that will help us to save the created account
+        test_save test case to test if the created account is saved into the account list
         '''
         self.newUser.saveAccount()
         self.assertEqual(len(UserData.createAccount),1)
+    
+    def tearDown(self):
+        '''
+        Function to clean up after each test case has run
+        '''
+        UserData.createAccount = []
+
+    def test_save_multiple_users(self):
+        """
+        test_save_multiple_users to check if we can save multiple usernames to our user_names
+        """
+        self.newUser.saveAccount()
+        test_userData = UserData("Aggy","Reina","aggyreina@gmail.com","aggy","12345")
+        test_userData.saveAccount()
+        self.assertEqual(len(UserData.createAccount),2)
+
+    
 
 if __name__ == '__main__':
     unittest.main()
