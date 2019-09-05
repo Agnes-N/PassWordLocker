1f6d51e78e0810a1fa2631bcbf6fe8761492c6d0
GraceUmutesi <tesigrace98@gmail.com>
2019-09-05 16:03:08 +0200

    creates codes to test initialization of cresention

---
+++ test_CredentialData.py
@@ -0,0 +1,19 @@
+import unittest
+
+from CredentialData import CredentialData
+
+
+class TestCredentials(unittest.TestCase):
+    def setUp(self):
+        """
+        setUp method
+        """
+        self.new_credential = CredentialData("instagram", "reina", "12345") 
+
+    def test_init(self):
+        """
+        testing initialization
+        """
+        self.assertEqual(self.new_credential.platform, "instagram")
+        self.assertEqual(self.new_credential.username, "reina")
+        self.assertEqual(self.new_credential.password, "12345")
